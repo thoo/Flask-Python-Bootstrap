0fc3d285c59be0519c23d3fc3c8c79a6a3f714a7
thoo <theinhtay@gmail.com>
2017-02-24 14:49:56 -0800

    Update https

--- app1.py
+++ app1.py
@@ -8,7 +8,7 @@ from werkzeug.contrib.fixers import ProxyFix
 app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app)
 app.config.update(dict(
-    SECRET_KEY = 'your_secret_key',
+    SECRET_KEY = ';aksjdflkjsdlkfjal;ksdjf;lkasjdfkljl;aksjdi43859oijkfiuojasdknmuviouewrmnnkjlasdhiewuhlgm,/mvm',
     CSRF_ENABLED = True,
 ))
 
